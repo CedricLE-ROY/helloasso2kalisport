bfa7ccef99c233621780ec25a000fb999ace3c32
Cédric LE ROY <108978496+CedricLE-ROY@users.noreply.github.com>
2025-07-17 20:25:40 +0200

    Add fallback content to frontend index

--- frontend/src/frontend.rs
+++ frontend/src/frontend.rs
@@ -3,8 +3,10 @@ use yew::prelude::*;
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
-        <div>
-            { "Bienvenue sur HelloAsso2Kalisport !" }
+        <div class="container">
+            <h1>{ "HelloAsso2Kalisport" }</h1>
+            <p>{ "Bienvenue ! Cette application vous permettra d'exporter vos inscriptions HelloAsso vers Kalisport." }</p>
+            <button>{ "Commencer" }</button>
         </div>
     }
 }
