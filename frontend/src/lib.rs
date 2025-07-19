43fdbc6747f0931f016aeb427ccaf8a447615cc5
Cédric LE ROY <108978496+CedricLE-ROY@users.noreply.github.com>
2025-07-19 14:42:21 +0200

    Organize frontend with pages and app module

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,6 +1,12 @@
-fn main() {
+use wasm_bindgen::prelude::*;
+
+/// Entry point called by the WASM runtime
+#[wasm_bindgen(start)]
+pub fn main() {
     wasm_logger::init(wasm_logger::Config::default());
-    yew::Renderer::<frontend::App>::new().render();
+    yew::Renderer::<app::App>::new().render();
 }
 
-mod frontend;
+mod app;
+pub mod pages;
+pub mod routes;
